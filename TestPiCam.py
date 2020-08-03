7ba95da538196abd996708cfe6dffbf5b1f93f4b
Brisbane Robotics Club <59764871+brisbaneroboticsclub@users.noreply.github.com>
2020-08-03 18:26:13 +1000

    Add files via upload

---
+++ TestPiCam.py
@@ -0,0 +1,26 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+'''
+http://brisbaneroboticsclub.id.au/raspberry-pi-camera/
+'''
+
+print('Open CV version :',cv2.__version__)
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+ 
+# display the image on screen and wait for a keypress
+cv2.imshow("Image", image)
+cv2.waitKey(0)
